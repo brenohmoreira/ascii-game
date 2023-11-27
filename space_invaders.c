baf96e5a4a266c619ad422d6195333a24b039827
Breno Moreira <breno.am2004@gmail.com>
2023-11-27 20:24:36 -0300

    inserção de nomes de usuários e menu

--- space_invaders.c
+++ space_invaders.c
@@ -9,19 +9,6 @@
 #define ROWS 10
 #define COLUMNS 40
 
-/*
-
-usamos threads quando queremos executar várias funções de uma vez só
-caso, por exemplo, criemos 3 threads, as 3 threads serão executadas ao mesmo tempo
-
-- cria uma thread, salva o endereço em threads[0] e passa a rotina func (o que a thread deve fazer)
-pthread_create(&(threads[0]), NULL, func, NULL);
-
-- inicia a thread
-pthread_join(threads[0], NULL);
-
-*/
-
 char map[ROWS][COLUMNS];
 int currentCol = 0, currentRow = 8, score = 0, attackRow = 0, attackColumn, gameStatus = 0, numAttacks = 0, counterEnemy = 0, counterAttack = 0;
 
@@ -31,23 +18,31 @@ pthread_t thread_player_action;
 pthread_t thread_invasion;
 pthread_mutex_t map_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// Structs
-typedef struct {
-    char name[MAX_NAME];
-    int score;
-} Player;
 
+// prototypes
 void reloadMap();
+void defeatScreen();
+void menu();
 void startGame();
 void *playerAction();
 void *invasion();
 void *enemy();
 
+// structs
+typedef struct {
+    char name[MAX_NAME];
+    int score;
+} Player;
+
 Player playerData();
 
-// Functions
+// functions
 Player playerData() {
     Player player;
+
+    getchar();
+
+    system("clear");
 
     printf("Enter your nickname: ");
     fgets(player.name, MAX_NAME, stdin);
@@ -65,6 +60,8 @@ void reloadMap() {
 
     clear();
 
+    printw("Status: %d\n", score);
+
     for(int i = 0; i < ROWS; i++) {
         for(int j = 0; j < COLUMNS; j++) {
             printw("%c ", map[i][j]);
@@ -72,15 +69,56 @@ void reloadMap() {
         printw("\n");
     }
 
-    // refresh screen
     refresh();
 }
 
+void menu() {
+    int op;
+
+    do {
+        system("clear");
+
+        printf("-------------------------------------------------\n|\t\tSPACE INVADERS\t\t\t|\n-------------------------------------------------\n");
+        printf("1 - Start\n2 - Players\n3 - Exit\nEnter option: ");
+        scanf("%d", &op);
+
+        switch(op) {
+            case 1: {
+                Player player = playerData();
+
+                FILE *players_list;
+                players_list = fopen("player.txt", "a");
+                fprintf(players_list, strcat(player.name, "\n"));
+
+                fflush(players_list);
+                fclose(players_list);
+
+                startGame();
+
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                exit(1);
+
+                break;
+            }
+        }
+    }
+    while(op < 5 && op > 1);
+}
+
+void defeatScreen() {
+    system("clear");
+}
+
 void startGame() {
-    initscr(); // inicializa o modo de tela ncurses
-    cbreak(); // desabilita linha de buffer
-    noecho(); // teclas digitadas não são exibidas
-    keypad(stdscr, TRUE); // permite entrada de caracteres especiais
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
 
     gameStatus = 1;
 
@@ -111,11 +149,12 @@ void startGame() {
     pthread_join(thread_invasion, NULL);
 }
 
+// thread rotines
 void *enemy(void *arg) {
     int columnEnemy = 1 + (rand() % 38);
     int rowEnemy;
 
-    for (rowEnemy = 0; rowEnemy < ROWS - 1; rowEnemy++) {
+    for (rowEnemy = 0; rowEnemy < ROWS - 1 && gameStatus == 1; rowEnemy++) {
         if(map[rowEnemy][columnEnemy] == ' ') {
             if(rowEnemy != 0 && rowEnemy != ROWS - 1) {
                 map[rowEnemy - 1][columnEnemy] = ' ';
@@ -132,9 +171,16 @@ void *enemy(void *arg) {
             map[rowEnemy - 1][columnEnemy] = 'X';
 
             reloadMap();
+
             pthread_exit(NULL);
         }
     }
+
+    gameStatus = 0;
+
+    defeatScreen();
+
+    pthread_exit(NULL);
 }
 
 void *invasion(void *arg) {
@@ -181,9 +227,7 @@ void *attack(void *arg) {
 }
 
 void *playerAction(void *arg) {
-    while(1) {
-        //printw("%d", gameStatus);
-        printw("Status: %d\n", score);
+    while(gameStatus) {
         switch(getch()) {
             case KEY_LEFT:
             {
@@ -207,7 +251,6 @@ void *playerAction(void *arg) {
             }
             case 32:
             {
-                // Para garantir que cada thread tenha seu ataque, não se usa attackRow e attackColumn diretamente
                 int attackParams[2];
 
                 attackRow = currentRow - 1;
@@ -224,18 +267,14 @@ void *playerAction(void *arg) {
 
         reloadMap();
     }
+
+    pthread_exit(NULL);
 }
 
-int main() {
-    startGame();
 
-    //Player player = playerData();
-    // Save player name
-    /*
-    FILE *players_list;
-    players_list = fopen("player.txt", "a");
-    fprintf(players_list, player.name);
-    */
+// main function
+int main() {
+    menu();
 
     return 1;
 }
